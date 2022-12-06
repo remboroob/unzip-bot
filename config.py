75594a41009b65e8c45777a38661f1d7eb9f5cd6
ismhazel <is.hazel@lagh-univ.dz>
2022-12-06 18:08:52 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config:
     APP_ID = int(os.environ.get("16092439"))
     API_HASH = os.environ.get("2583b8bdc2ff7e6d4acca21ae33411cd")
     BOT_TOKEN = os.environ.get("5732579204:AAH0nB0d4z63RRZWCpyrvdS2m4xCdd76UIM")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    MONGODB_URL = os.environ.get("MONGODB_URL")
+    LOGS_CHANNEL = int(os.environ.get("1001762533714"))
+    MONGODB_URL = os.environ.get("https://winter.pn2leechrm12.workers.dev/0:/rembo_PN/rpd%20amazon/")
     BOT_OWNER = int(os.environ.get("1993084922"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
